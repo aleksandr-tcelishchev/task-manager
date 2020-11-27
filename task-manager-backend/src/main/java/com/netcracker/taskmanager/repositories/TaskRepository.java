ecf70561d33421dd70e6bd99239d4febddc75541
ManDrakeRo0t <danik21.01.01@gmail.com>
2020-11-27 13:24:27 +0400

    added Entity and Repositories (edit primary key type)

--- task-manager-backend/src/main/java/com/netcracker/taskmanager/repositories/TaskRepository.java
+++ task-manager-backend/src/main/java/com/netcracker/taskmanager/repositories/TaskRepository.java
@@ -3,8 +3,7 @@ package com.netcracker.taskmanager.repositories;
 import com.netcracker.taskmanager.entity.Task;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.UUID;
 
-public interface TaskRepository extends MongoRepository<Task, UUID> {
+public interface TaskRepository extends MongoRepository<Task, String> {
 
 }
--- task-manager-backend/src/main/java/com/netcracker/taskmanager/repositories/UserRepository.java
+++ task-manager-backend/src/main/java/com/netcracker/taskmanager/repositories/UserRepository.java
@@ -3,8 +3,7 @@ package com.netcracker.taskmanager.repositories;
 import com.netcracker.taskmanager.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.UUID;
 
-public interface UserRepository extends MongoRepository<User, UUID> {
+public interface UserRepository extends MongoRepository<User, String> {
 
 }
